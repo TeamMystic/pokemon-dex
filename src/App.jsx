507c86aa4ba2ed4d2dfe33ef7746dd725bbddc63
aXLan666 <angaragilang266@gmail.com>
2025-04-21 06:04:56 +0700

    chore: bersihkan code tidak di gunakan App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useParams } from "react-router-dom";
 
 function App() {
-  const { query } = useParams();
   const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(query);
-  }, []);
 
   return (
     <>
