1c221d91d89129b457137feec7fb80ac05d095ca
aXLan666 <angaragilang266@gmail.com>
2025-04-21 06:03:08 +0700

    feat: tambah package react-router-dom untuk router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useParams } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { query } = useParams();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(query);
+  }, []);
 
   return (
     <>
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
