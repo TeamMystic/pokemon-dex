a6278e28e40e0a06d47fbd4604db124524de47bf
aXLan666 <angaragilang266@gmail.com>
2025-04-25 15:49:25 +0700

    fix: fix on bottom load more cards pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ function App() {
   const [selectedgen, setSelectedgen] = useState([]);
   const scrollContainer = useRef();
   const [toBottom, settoBottom] = useState(false);
+  const [scrollTop, setscrollTop] = useState(0);
   const { dataQuery, search, type, attr } = useSearchPokemon(query, toBottom);
   const [loopUse, setloopUse] = useState({
     search: false,
@@ -84,12 +85,19 @@ function App() {
   const navigate = useNavigate();
 
   const handleScroll = () => {
+    setscrollTop(scrollContainer.current.scrollTop);
     Math.ceil(
       scrollContainer.current.scrollTop + scrollContainer.current.clientHeight
     ) >= scrollContainer.current.scrollHeight
       ? settoBottom(true)
       : settoBottom(false);
   };
+
+  useEffect(() => {
+    Math.ceil(
+      scrollContainer.current.scrollTop + scrollContainer.current.clientHeight
+    );
+  }, [scrollTop]);
 
   useEffect(() => {
     const navigasiQuery = () => {
@@ -128,179 +136,149 @@ function App() {
 
   return (
     <>
-      <Navbar />
       <div
         ref={scrollContainer}
         onScroll={handleScroll}
-        className="overflow-y-scroll h-screen bg-gray-900 px-12 pt-6"
+        className="overflow-y-scroll h-screen bg-gray-900"
       >
-        <h1 className="text-2xl pb-3 font-bold text-white">Pokémon Species</h1>
+        <Navbar />
+        <div className="h-fit bg-gray-900 px-12 pt-6">
+          <h1 className="text-2xl pb-3 font-bold text-white">
+            Pokémon Species
+          </h1>
 
-        <div className="flex flex-wrap gap-x-2 gap-y-2.5 items-center">
-          {/* Search Pokémon */}
-          <label htmlFor="searchPokemon"></label>
-          <input
-            id="searchPokemon"
-            type="search"
-            placeholder="Search pokémon"
-            className="rounded-md border bg-B-dark px-3 py-2 bg-gray-800 text-white placeholder:text-white"
-            maxLength="15"
-            value={searchPokemon}
-            onChange={(e) => setsearchPokemon(e.target.value)}
-          />
-          <div className="relative">
-            <div className="top-24 w-52 text-right">
-              {/* {JSON.stringify(selectedgen.join("."))} */}
+          <div className="flex flex-wrap gap-x-2 gap-y-2.5 items-center">
+            {/* Search Pokémon */}
+            <label htmlFor="searchPokemon"></label>
+            <input
+              id="searchPokemon"
+              type="search"
+              placeholder="Search pokémon"
+              className="rounded-md border bg-B-dark px-3 py-2 bg-gray-800 text-white placeholder:text-white"
+              maxLength="15"
+              value={searchPokemon}
+              onChange={(e) => setsearchPokemon(e.target.value)}
+            />
+            <div className="relative">
+              <div className="top-24 w-52 text-right">
+                {/* {JSON.stringify(selectedgen.join("."))} */}
 
-              <Listbox value={selectedgen} onChange={setSelectedgen} multiple>
-                <ListboxButton className="text-white px-3 py-1 bg-gray-700 rounded-sm text-xl">
-                  Choose Gen
-                </ListboxButton>
-                <ListboxOptions
-                  anchor="bottom"
-                  className="w-32 px-2 py-2 bg-gray-800"
-                >
-                  {genList.map((gen, index) => (
-                    <ListboxOption
-                      key={index}
-                      value={gen}
-                      className="px-1 py-1 rounded data-focus:bg-gray-900 text-white cursor-pointer"
-                    >
-                      Gen {gen}
-                    </ListboxOption>
-                  ))}
-                </ListboxOptions>
-              </Listbox>
-
-              <Menu>
-                <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-700 data-open:bg-gray-700">
-                  Any Generations
-                  <ChevronDownIcon className="size-4 fill-white/60" />
-                </MenuButton>
-
-                <MenuItems
-                  transition
-                  anchor="bottom end"
-                  className="w-52 origin-top-right rounded-xl border border-white/5 bg-black p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
-                >
-                  <MenuItem>
-                    <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10">
-                      Gen I
-                    </button>
-                  </MenuItem>
-                  <MenuItem>
-                    <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10">
-                      Gen II
-                    </button>
-                  </MenuItem>
-                  <MenuItem>
-                    <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10">
-                      Gen III
-                    </button>
-                  </MenuItem>
-                  <MenuItem>
-                    <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10">
-                      Gen IV
-                    </button>
-                  </MenuItem>
-                </MenuItems>
-              </Menu>
+                <Listbox value={selectedgen} onChange={setSelectedgen} multiple>
+                  <ListboxButton className="text-white px-3 py-1 bg-gray-700 rounded-sm text-xl">
+                    Choose Gen
+                  </ListboxButton>
+                  <ListboxOptions
+                    anchor="bottom"
+                    className="w-32 px-2 py-2 bg-gray-800"
+                  >
+                    {genList.map((gen, index) => (
+                      <ListboxOption
+                        key={index}
+                        value={gen}
+                        className="px-1 py-1 rounded data-focus:bg-gray-900 text-white cursor-pointer"
+                      >
+                        Gen {gen}
+                      </ListboxOption>
+                    ))}
+                  </ListboxOptions>
+                </Listbox>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="pt-12 mb-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
-          {loopUse.attr && (
-            <div class="w-full rounded-xl bg-gradient-to-br from-green-600 to-green-800 text-white p-4 relative overflow-hidden shadow-lg">
-              {/* <!-- Background Pokéball watermark --> */}
-              <div class="absolute right-2 bottom-2 opacity-10 text-[9rem] select-none pointer-events-none">
-                ⬤
-              </div>
-              {/* <!-- Pokémon Name and Number --> */}
-              <div class="relative z-10">
-                <h2 class="text-2xl font-bold">Bulbasaur</h2>
-                <p class="text-sm text-gray-200">#0001</p>
-                <p class="italic text-sm text-gray-300 mt-1">Grass, Poison</p>
-
-                {/* <!-- Type icons --> */}
-                <div class="flex space-x-2 mt-2">
-                  <span class="w-4 h-4 rounded-full bg-green-400"></span>
-                  <span class="w-4 h-4 rounded-full bg-purple-500"></span>
+          <div className="pt-12 pb-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+            {loopUse.attr && (
+              <div class="w-full rounded-xl bg-gradient-to-br from-green-600 to-green-800 text-white p-4 relative overflow-hidden shadow-lg">
+                {/* <!-- Background Pokéball watermark --> */}
+                <div class="absolute right-2 bottom-2 opacity-10 text-[9rem] select-none pointer-events-none">
+                  ⬤
                 </div>
-              </div>
-              {/* <!-- Pokémon Image --> */}
-              <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="Bulbasaur"
-                class="absolute bottom-2 right-2 h-[100px] z-0 drop-shadow-xl"
-              />
-            </div>
-          )}
-          {loopUse.type && (
-            <div class="w-full rounded-xl bg-gradient-to-br from-green-600 to-green-800 text-white p-4 relative overflow-hidden shadow-lg">
-              {/* <!-- Background Pokéball watermark --> */}
-              <div class="absolute right-2 bottom-2 opacity-10 text-[9rem] select-none pointer-events-none">
-                ⬤
-              </div>
-              {/* <!-- Pokémon Name and Number --> */}
-              <div class="relative z-10">
-                <h2 class="text-2xl font-bold">Bulbasaur</h2>
-                <p class="text-sm text-gray-200">#0001</p>
-                <p class="italic text-sm text-gray-300 mt-1">Grass, Poison</p>
+                {/* <!-- Pokémon Name and Number --> */}
+                <div class="relative z-10">
+                  <h2 class="text-2xl font-bold">Bulbasaur</h2>
+                  <p class="text-sm text-gray-200">#0001</p>
+                  <p class="italic text-sm text-gray-300 mt-1">Grass, Poison</p>
 
-                {/* <!-- Type icons --> */}
-                <div class="flex space-x-2 mt-2">
-                  <span class="w-4 h-4 rounded-full bg-green-400"></span>
-                  <span class="w-4 h-4 rounded-full bg-purple-500"></span>
-                </div>
-              </div>
-              {/* <!-- Pokémon Image --> */}
-              <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="Bulbasaur"
-                class="absolute bottom-2 right-2 h-[100px] z-0 drop-shadow-xl"
-              />
-            </div>
-          )}
-          {loopUse.search ? (
-            <>
-              {search.map((data) => {
-                return (
-                  <div class="w-full rounded-xl bg-gradient-to-br from-green-600 to-green-800 text-white p-4 relative overflow-hidden shadow-lg">
-                    {/* <!-- Background Pokéball watermark --> */}
-                    <div class="absolute right-2 bottom-2 opacity-10 text-[9rem] select-none pointer-events-none">
-                      ⬤
-                    </div>
-                    {/* <!-- Pokémon Name and Number --> */}
-                    <div class="relative z-10">
-                      <h2 class="text-2xl font-bold">{data.name}</h2>
-                      <p class="text-sm text-gray-200">#0001</p>
-                      <p class="italic text-sm text-gray-300 mt-1">
-                        {data.types.map((ty) => {
-                          let types = "";
-                          types += ` ${ty.type.name}`;
-                          return <>{types}</>;
-                        })}
-                        {/* Grass, Poison */}
-                      </p>
-
-                      {/* <!-- Type icons --> */}
-                      <div class="flex space-x-2 mt-2">
-                        <span class="w-4 h-4 rounded-full bg-green-400"></span>
-                        <span class="w-4 h-4 rounded-full bg-purple-500"></span>
-                      </div>
-                    </div>
-                    {/* <!-- Pokémon Image --> */}
-                    <img
-                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`}
-                      alt="Bulbasaur"
-                      class="absolute bottom-2 right-2 h-[100px] z-0 drop-shadow-xl"
-                    />
+                  {/* <!-- Type icons --> */}
+                  <div class="flex space-x-2 mt-2">
+                    <span class="w-4 h-4 rounded-full bg-green-400"></span>
+                    <span class="w-4 h-4 rounded-full bg-purple-500"></span>
                   </div>
-                );
-              })}
-            </>
-          ) : null}
+                </div>
+                {/* <!-- Pokémon Image --> */}
+                <img
+                  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                  alt="Bulbasaur"
+                  class="absolute bottom-2 right-2 h-[100px] z-0 drop-shadow-xl"
+                />
+              </div>
+            )}
+            {loopUse.type && (
+              <div class="w-full rounded-xl bg-gradient-to-br from-green-600 to-green-800 text-white p-4 relative overflow-hidden shadow-lg">
+                {/* <!-- Background Pokéball watermark --> */}
+                <div class="absolute right-2 bottom-2 opacity-10 text-[9rem] select-none pointer-events-none">
+                  ⬤
+                </div>
+                {/* <!-- Pokémon Name and Number --> */}
+                <div class="relative z-10">
+                  <h2 class="text-2xl font-bold">Bulbasaur</h2>
+                  <p class="text-sm text-gray-200">#0001</p>
+                  <p class="italic text-sm text-gray-300 mt-1">Grass, Poison</p>
+
+                  {/* <!-- Type icons --> */}
+                  <div class="flex space-x-2 mt-2">
+                    <span class="w-4 h-4 rounded-full bg-green-400"></span>
+                    <span class="w-4 h-4 rounded-full bg-purple-500"></span>
+                  </div>
+                </div>
+                {/* <!-- Pokémon Image --> */}
+                <img
+                  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                  alt="Bulbasaur"
+                  class="absolute bottom-2 right-2 h-[100px] z-0 drop-shadow-xl"
+                />
+              </div>
+            )}
+            {loopUse.search ? (
+              <>
+                {search.map((data) => {
+                  return (
+                    <div class="w-full rounded-xl bg-gradient-to-br from-green-600 to-green-800 text-white p-4 relative overflow-hidden shadow-lg">
+                      {/* <!-- Background Pokéball watermark --> */}
+                      <div class="absolute right-2 bottom-2 opacity-10 text-[9rem] select-none pointer-events-none">
+                        ⬤
+                      </div>
+                      {/* <!-- Pokémon Name and Number --> */}
+                      <div class="relative z-10">
+                        <h2 class="text-2xl font-bold">{data.name}</h2>
+                        <p class="text-sm text-gray-200">#0001</p>
+                        <p class="italic text-sm text-gray-300 mt-1">
+                          {data.types.map((ty) => {
+                            let types = "";
+                            types += ` ${ty.type.name}`;
+                            return <>{types}</>;
+                          })}
+                          {/* Grass, Poison */}
+                        </p>
+
+                        {/* <!-- Type icons --> */}
+                        <div class="flex space-x-2 mt-2">
+                          <span class="w-4 h-4 rounded-full bg-green-400"></span>
+                          <span class="w-4 h-4 rounded-full bg-purple-500"></span>
+                        </div>
+                      </div>
+                      {/* <!-- Pokémon Image --> */}
+                      <img
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`}
+                        alt="Bulbasaur"
+                        class="absolute bottom-2 right-2 h-[100px] z-0 drop-shadow-xl"
+                      />
+                    </div>
+                  );
+                })}
+              </>
+            ) : null}
+          </div>
         </div>
       </div>
     </>
